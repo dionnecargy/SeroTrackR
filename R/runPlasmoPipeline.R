f4fa9076c0ad9df580c3107349e5530d313f34ff
Dionne <dionne.argy@gmail.com>
2025-09-24 09:27:47 +1000

    Updated runPlasmoPipeline() by adding Pv classification, also added new function classifyPv.R to classify Pv from the mfi_to_rau_output from the Pk/Pf/Pv pipeline as the column names slightly differ

--- R/runPlasmoPipeline.R
+++ R/runPlasmoPipeline.R
@@ -6,12 +6,32 @@
 #' @param panel Panel of Pk/Pf/Pv antigens. Default = "panel1".
 #' @param std_point Standard Point Curve: 5 = 5-point curve, 10 = 10-point curve. Value is an integer.
 #' @param experiment_name User-input experiment name. Default: "experiment1".
+#' @param algorithm_type User-selected algorithm choice:
+#' - "antibody_model" (PvSeroTaT model; default), or
+#' - "antibody_model_excLF016" (PvSeroTaT excluding LF016).
+#' @param sens_spec User-selected Sensitivity/Specificity threshold:
+#' - "maximised" (default),
+#' - "85\% sensitivity",
+#' - "90\% sensitivity",
+#' - "95\% sensitivity",
+#' - "85\% specificity",
+#' - "90\% specificity".
+#' - "95\% specificity".
 #'
 #' @return A data frame containing the MFI and RAU Dilution values for each sample, QC plots for standard curve, bead counts and blanks.
 #' @export
 #'
 #' @author Dionne Argyropoulos
-runPlasmoPipeline <- function(raw_data, platform = "magpix", plate_layout, panel = "panel1", std_point, experiment_name = "experiment1"){
+runPlasmoPipeline <- function(
+    raw_data,
+    platform = "magpix",
+    plate_layout,
+    panel = "panel1",
+    std_point,
+    experiment_name = "experiment1",
+    algorithm_type = "antibody_model",
+    sens_spec = "maximised"
+  ){
 
   #############################################################
   # Step 1: Reading in Raw Data
@@ -51,13 +71,20 @@ runPlasmoPipeline <- function(raw_data, platform = "magpix", plate_layout, panel
   message("MFI to RAU conversion completed.")
 
   #############################################################
+  # Step 5: Perform Pv classification
+  #############################################################
+  Pv_classified <- classifyPv(mfi_outputs, algorithm_type, sens_spec, getCountsQC_output)
+  message("Pv classification completed.")
+
+  #############################################################
   # Outputs
   #############################################################
   return(list(
     std_curve = stdcurve_plot,
     bead_counts = plateqc_plot,
     blanks = blanks_plot,
-    mfi_outputs = mfi_outputs
+    mfi_outputs = mfi_outputs,
+    pv_classification = Pv_classified
   ))
 
 }
