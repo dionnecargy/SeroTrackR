fb74c693d6e1524e68aabfa915c90fe3951aa69f
CB <cb@Caitlins-MacBook-Pro.local>
2026-02-16 14:20:46 +0930

    separation for standard curve accommodates ETH/PK separated by a space and then entered with or without brackets

--- R/plotStds_PkPfPv.R
+++ R/plotStds_PkPfPv.R
@@ -105,7 +105,7 @@ plotStds_PkPfPv <- function(sero_data, experiment_name){
     tidyr::pivot_longer(-c(Sample, Beads, Plate), names_to = "Antigen", values_to = "MFI") %>%
     dplyr::mutate(
       Plate = factor(Plate, levels = unique(Plate[order(as.numeric(str_extract(Plate, "\\d+")))])), # reorder by plate number
-      # Beads = stringr::str_extract(Beads, "(?<=\\().+?(?=\\))"),
+      Beads = stringr::str_extract(Beads, "[A-Z]+"), # ETH (ETH) may sometimes be entered differently (in brackets or not) - this should just keep the letters PK or ETH that we want
       Sample = factor(Sample, levels = unique(Sample[order(as.numeric(str_extract(Sample, "\\d+")))])), # reorder by standard curve number
       MFI = as.numeric(MFI)
     ) %>%
